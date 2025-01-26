25a34112ad6db4cd001de596927d5c8d6d6b8de0
theliljannah@gmail.com <j4nn47@kali>
2025-01-26 14:23:29 +0100

    30 Days of Bash in progress.

--- Exercises/day23.sh
+++ Exercises/day23.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 git add .
 
-read -p "Enter commit message: " message
-git commit -m "$msg"
-
+git commit -m "30 Days of Bash in progress."
 git pull
 git push
 
 
-git tag -a v1.0 -m "Version 2.0"
+git tag -a v2.0 -m "Version 2.0"
